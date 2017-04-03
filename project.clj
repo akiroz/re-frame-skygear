c8b43341aa90e93ae966cc7e9564ab3dd5afb431
akiroz <akiroz.vectis@gmail.com>
2017-04-03 15:53:58 +0800

    bump dep versions

--- project.clj
+++ project.clj
@@ -1,17 +1,16 @@
-(defproject akiroz.re-frame/skygear "0.1.4-SNAPSHOT"
+(defproject akiroz.re-frame/skygear "0.1.5-SNAPSHOT"
   :description "re-frame fx & cofx handlers for the skygear BaaS"
   :url "https://github.com/akiroz/re-frame-skygear"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[cljsjs/skygear "0.20.0-0"]
-                 [binaryage/oops "0.4.0"]]
+  :dependencies [[cljsjs/skygear "0.22.2-0"]
+                 [binaryage/oops "0.5.3"]]
   
   :profiles {:test {:plugins [[lein-cljsbuild "1.1.4"]
                               [lein-doo "0.1.7"]]
-                    :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
-                                   [org.clojure/clojurescript "1.9.229"]
-                                   [org.clojure/core.async "0.2.391"]
-                                   [binaryage/oops "0.4.0"]
+                    :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
+                                   [org.clojure/clojurescript "1.9.494"]
+                                   [org.clojure/core.async "0.3.442"]
                                    [re-frame "0.8.0"]
                                    [reagent "0.6.0"]
                                    ]
